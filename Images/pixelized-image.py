ea306dc4b10f1d498ab22461a08482aca2ea6cc6
snuq <snuq@snuq.com>
2024-10-16 14:45:10 -0700

    Update pixelated image example

--- Images/pixelized-image.py
+++ Images/pixelized-image.py
@@ -1,13 +1,8 @@
-from kivy.app import App
 from kivy.uix.image import Image
 
 class PixelImage(Image):
     def on_texture(self, *_):
-        self.texture.mag_filter = "nearest"
-        self.texture.min_filter = "nearest"
+        self.texture.mag_filter = "nearest"  #filter when image is larger than original
+        self.texture.min_filter = "nearest"  #filter when image is smaller than original
 
-class Test(App):
-    def build(self):
-        return PixelImage(source='W_Pawn.png', allow_stretch=True)
-
-Test().run()
+from kivy import app; app.runTouchApp(PixelImage(source='small.bmp', fit_mode='contain'))
