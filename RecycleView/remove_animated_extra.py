059ee63a64c639892b5a1227e6392a1bd5cc4193
hudson <snuq@snuq.com>
2024-11-27 14:00:56 -0800

    added new recycleview remove animated example

---
+++ RecycleView/remove_animated_extra.py
@@ -0,0 +1,91 @@
+"""Example showing how to remove elements from a RecycleView.  This version includes more advanced animations for element removal."""
+from kivy.app import App
+from kivy.animation import Animation
+from kivy.properties import ObjectProperty, StringProperty, ListProperty, BooleanProperty, AliasProperty, NumericProperty, DictProperty
+from kivy.lang.builder import Builder
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.boxlayout import BoxLayout
+KV = """
+<RecycleItem>:
+    orientation: 'horizontal'
+    Label:
+        text: root.text
+    Button:
+        text: 'Remove'
+        on_press: root.remove()
+
+RecycleView:
+    id: rv
+    data: app.data
+    viewclass: 'RecycleItem'
+    AnimatedRecycleBoxLayout:
+        spacing: 10
+        default_size: None, dp(80)
+        default_size_hint: 1, None
+        orientation: 'vertical'
+        size_hint_y: None
+        height: self.minimum_height
+        remove_callback: app.delete_element
+"""
+
+
+class AnimatedRecycleBoxLayout(RecycleBoxLayout):
+    remove_callback = ObjectProperty()  #needs to be set to a function that will actually remove this index from the data list and trigger a refresh
+    remove_length = NumericProperty(.25)
+    _remove_animation = ObjectProperty(allownone=True)
+    _move_animations = ListProperty()
+    _original_data = []
+    _remove_index = NumericProperty()
+
+    def remove_element(self, element):
+        if not self._remove_animation:
+            self._original_data = []
+            self._remove_index = element.index
+            for child in self.children:
+                self._original_data.append([child, child.pos, child.opacity])  #store un-animated data
+                if child.index > element.index:  #child element is lower in list, needs to be animated to show removal
+                    anim = Animation(y=child.y + element.height, duration=self.remove_length)
+                    anim.start(child)
+                    self._move_animations.append(anim)
+            self._remove_animation = Animation(opacity=0, duration=self.remove_length, pos=(element.pos[0]-element.width, element.pos[1]))
+            self._remove_animation.start(element)  #animate a fly-out and opacity fade on element being removed
+            self._remove_animation.bind(on_complete=self.remove_finish)
+
+    def remove_finish(self, *_):
+        for move_animation in self._move_animations:  #just in case some animations end up going an extra frame, cancel them all
+            move_animation.cancel_all(widget=None)
+        for data in self._original_data:  #need to restore non-animated values before updating the recycleview or elements will be positioned wrong
+            child, pos, opacity = data
+            child.pos = pos
+            child.opacity = opacity
+        self._original_data = []
+        self._move_animations = []
+        self._remove_animation = None
+        self.remove_callback(self._remove_index)
+
+
+class RecycleItem(RecycleDataViewBehavior, BoxLayout):
+    index = NumericProperty()
+    text = StringProperty()
+
+    def refresh_view_attrs(self, rv, index, data):
+        self.index = index
+        return super(RecycleItem, self).refresh_view_attrs(rv, index, data)
+
+    def remove(self):
+        self.parent.remove_element(self)
+
+
+class Test(App):
+    data = ListProperty()
+
+    def delete_element(self, index):
+        self.data.pop(index)
+
+    def build(self):
+        self.data = [{"text": "Label "+str(x), 'owner': self} for x in range(20)]
+        return Builder.load_string(KV)
+
+if __name__ == '__main__':
+    Test().run()
